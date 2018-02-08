a7bc6f3d5f2e53ce369ff90d1a834490bdc06256
BenCmjn <benjamin.calmejane@gmail.com>
2018-02-08 15:17:05 +0100

    bootstrap is here

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  root 'events#index'
+  resources :users
+
+  get 'users/new'
+  get 'users/show'
+
+  get 'events/index'
+
+  get '/signup',  to: 'users#new'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+
+  get	 '/logout',  to: 'sessions#destroy'
+  delete '/logout',  to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
